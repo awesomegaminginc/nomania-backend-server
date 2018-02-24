da4a58f3dc1b8cf11bdefdfb20ffcf1a65e65b7e
Manav Bokinala <manav.bokinala@gmail.com>
2018-02-24 12:58:07 -0800

    Changed 'Process' to 'process' in server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 var app = express();
-const port = Process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
